d4d9b97c64c8dca16533191fa2dd17ca7c68297d
gmekv <113336356+gmekv@users.noreply.github.com>
2023-05-03 11:37:29 +0400

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Giorgi Meqvabishvili on 03.05.23.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
